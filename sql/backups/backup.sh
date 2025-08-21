75212230a4861c9feb4870679754859426c96c4d
longotiziano <longo.tiziano.arg@gmail.com>
2025-08-21 12:23:46 +0000

    final touches

--- sql/backups/backup.sh
+++ sql/backups/backup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 
-DB_NAME="restaurants_db"
-USER="postgres"
-HOST="db"
-DATE=$(date +%Y%m%d_%H%M%S)
+export $(cat .env | xargs)
+
 DIR="$(cd "$(dirname "$0")" && pwd)"
-FILE="$DIR/${DB_NAME}_$DATE.backup"
+
+DATE=$(date +%Y%m%d_%H%M%S)
+FILE="$DIR/${POSTGRES_DB}_$DATE.backup"
 
 CONTAINER="stock_managment-db-1"
 
-docker exec $CONTAINER pg_dump -U $USER -F c -b -v -f /tmp/$DB_NAME_$DATE.backup $DB_NAME
-docker cp $CONTAINER:/tmp/$DB_NAME_$DATE.backup "$FILE"
-docker exec $CONTAINER rm /tmp/$DB_NAME_$DATE.backup
+docker exec $CONTAINER pg_dump -U $POSTGRES_USER -F c -b -v -f /tmp/${POSTGRES_DB}_$DATE.backup $POSTGRES_DB
+docker cp $CONTAINER:/tmp/${POSTGRES_DB}_$DATE.backup "$FILE"
+docker exec $CONTAINER rm /tmp/${POSTGRES_DB}_$DATE.backup
+
+echo "Backup generado en $FILE"
